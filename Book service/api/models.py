77adb3ad9cdd05a06371530467f052dbeb1076d0
Jaklin <jaklin.yanakieva@gmail.com>
2022-04-04 15:48:22 +0200

    updated project - no issues

--- Book service/api/models.py
+++
@@ -1,8 +0,0 @@
-from .database import Base
-import sqlalchemy as sa
-
-
-class Book(Base):
-    __tablename__ = "Book"
-    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-    title = sa.Column(sa.String(length=255), nullable=False)
