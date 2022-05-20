cbde62ff7cb5b5466748a76aad3d4e4cd5c95f5b
Zhaklin Yanakieva <428428@student.fontys.nl>
2022-05-20 12:21:54 +0200

    update of the docker-file (db ports added)

--- user-service/app/api/db_manager.py
+++ user-service/app/api/db_manager.py
@@ -2,11 +2,11 @@ from app.api.models import UserIn, UserOut, UserUpdate
 from app.api.db import users, database
 
 
-async def add_cast(payload: UserIn):
+async def add_user(payload: UserIn):
     query = users.insert().values(**payload.dict())
 
     return await database.execute(query=query)
 
-async def get_cast(id):
+async def get_user(id):
     query = users.select(users.c.id==id)
     return await database.fetch_one(query=query)
